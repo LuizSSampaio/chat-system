1df3e90a6d24d8a235ff24f687352edf67e97adf
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2024-08-18 08:44:12 -0300

    feat(server): :bug: Added check for socket connection
    Now when the server read the connection for a message it check if the connection still open

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -3,6 +3,37 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+bool check_if_socket_connected(int clientSocket)
+{
+    char buffer;
+    ssize_t result = recv(clientSocket, &buffer, 1, MSG_PEEK);
+
+    if (result == 0)
+    {
+        return false;
+    }
+    else if (result < 0)
+    {
+        if (errno == EWOULDBLOCK || errno == EAGAIN)
+        {
+            return true;
+        }
+        return false;
+    }
+    return true;
+}
+
+static void messenger(int clientSocket)
+{
+    char buffer[1024] = {0};
+    if (recv(clientSocket, buffer, sizeof(buffer), 0) == -1)
+    {
+        std::cerr << "Error reading socket" << std::endl;
+        return;
+    }
+    std::cout << "Message from client: " << buffer << std::endl;
+}
+
 int main(int argc, char const *argv[])
 {
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -44,7 +75,16 @@ int main(int argc, char const *argv[])
         else
         {
             std::cout << "Connection accepted" << std::endl;
-            break;
+            while (clientSocket)
+            {
+                if (!check_if_socket_connected(clientSocket))
+                {
+                    std::cout << "Connection lost" << std::endl;
+                    close(clientSocket);
+                    break;
+                }
+                messenger(clientSocket);
+            }
         }
     }
 
