9606b9806aae6867df1b2c724a017cd65e2b98f5
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2024-08-17 22:15:34 -0300

    feat(server): :sparkles: Socket connection

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello Server" << std::endl;
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1)
+    {
+        std::cerr << "Error creating socket" << std::endl;
+        return -1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;         // IPV4
+    serverAddr.sin_port = htons(1234);       // Port: 1234
+    serverAddr.sin_addr.s_addr = INADDR_ANY; // Address: 127.0.0.1
+
+    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+    {
+        std::cerr << "Error binding socket" << std::endl;
+        close(serverSocket);
+        return -1;
+    }
+
+    if (listen(serverSocket, 5) == -1)
+    {
+        std::cerr << "Error listening for connections" << std::endl;
+        close(serverSocket);
+        return -1;
+    }
+
+    sockaddr_in clientAddr;
+    socklen_t client_len = sizeof(clientAddr);
+    while (true)
+    {
+        int clientSocket = accept(serverSocket, (struct sockaddr *)&serverAddr, &client_len);
+        if (clientSocket == -1)
+        {
+            std::cerr << "Error accepting connection" << std::endl;
+            continue;
+        }
+        else
+        {
+            std::cout << "Connection accepted" << std::endl;
+            break;
+        }
+    }
+
+    close(serverSocket);
     return 0;
 }
